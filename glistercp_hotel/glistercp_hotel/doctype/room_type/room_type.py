c48872438768af7ac3734b50c5fe2b2e265dbf8a
Adesina Akinyemi <59088328+adesinasam@users.noreply.github.com>
2024-03-12 23:24:28 +0100

    Update

--- glistercp_hotel/glistercp_hotel/doctype/room_type/room_type.py
+++ glistercp_hotel/glistercp_hotel/doctype/room_type/room_type.py
@@ -3,27 +3,24 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class RoomType(Document):
-  def before_save(self):
-    self.create_item_group()
+    def before_save(self):
+        self.create_item_group()
 
-  def on_save(self):
-    pass
-
-  def create_item_group(self):
-  	if frappe.db.exists('Item Group', {'item_group_name': self.type}):
-       	# If the Room Type exists, update the item_group_name
-       	existing_item_group_name = frappe.get_doc('Item Group', {'item_group_name': self.type})
-       	existing_item_group_name.item_group_name = self.type
-       	existing_item_group_name.save()
-    else:
-       	# If the Room Type does not exist, create a new document and insert it
-       	item_group = frappe.new_doc('Item Group')
-       	item_group.item_group_name = self.type
-       	item_group.parent_item_group = "All Item Groups"
-       	item_group_name.insert()
+    def create_item_group(self):
+        if frappe.db.exists('Item Group', {'item_group_name': self.type}):
+            # If the Room Type exists, update the item_group_name
+            existing_item_group = frappe.get_doc('Item Group', {'item_group_name': self.type})
+            existing_item_group.item_group_name = self.type
+            existing_item_group.save()
+        else:
+            # If the Room Type does not exist, create a new document and insert it
+            item_group = frappe.new_doc('Item Group')
+            item_group.item_group_name = self.type
+            item_group.parent_item_group = "All Item Groups"
+            item_group.insert()
 
   
--- glistercp_hotel/glistercp_hotel/doctype/rooms/rooms.py
+++ glistercp_hotel/glistercp_hotel/doctype/rooms/rooms.py
@@ -3,29 +3,27 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class Rooms(Document):
-  def before_save(self):
-    self.create_item()
+    def before_save(self):
+        self.create_or_update_item()
 
-  def on_save(self):
-    pass
-
-  def create_item(self):
-  	if frappe.db.exists('Item', {'item_code': self.room_number}):
-       	# If the Room Type exists, update the item_group_name
-       	existing_item_name = frappe.get_doc('Item Group', {'item_group_name': self.room_number})
-       	existing_item_name.item_name = self.room_name
-       	existing_item_name.item_group = self.room_type
-       	existing_item_name.save()
-    else:
-       	# If the Room Type does not exist, create a new document and insert it
-       	item_name = frappe.new_doc('Item Group')
-       	item_name.item_code = self.room_number
-       	item_name.item_name = self.room_name
-       	item_name.item_group = self.room_type
-       	item_name.price = self.standard_rate
-       	item_name.is_stock_item = 0
-       	item_name.insert()
+    def create_or_update_item(self):
+        if frappe.db.exists('Item', {'item_code': self.room_number}):
+            # If the Room Type exists, update the item
+            existing_item = frappe.get_doc('Item', {'item_code': self.room_number})
+            existing_item.item_name = self.room_name
+            existing_item.item_group = self.room_type
+            existing_item.standard_rate = self.price
+            existing_item.save()
+        else:
+            # If the Room Type does not exist, create a new item
+            new_item = frappe.new_doc('Item')
+            new_item.item_code = self.room_number
+            new_item.item_name = self.room_name
+            new_item.item_group = self.room_type
+            new_item.standard_rate = self.price
+            new_item.is_stock_item = 0  # Assuming rooms are not stock items
+            new_item.insert()
