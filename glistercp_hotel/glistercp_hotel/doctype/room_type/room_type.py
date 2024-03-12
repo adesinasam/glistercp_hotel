e46e8da69a9ea06152c663c12eba93a5398862e2
Adesina Akinyemi <59088328+adesinasam@users.noreply.github.com>
2024-03-12 22:58:06 +0100

    Update files

--- glistercp_hotel/glistercp_hotel/doctype/room_type/room_type.py
+++ glistercp_hotel/glistercp_hotel/doctype/room_type/room_type.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2020, Havenir and contributors
+# Copyright (c) 2024, Glistercp and Contributors
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
@@ -7,4 +7,23 @@ from __future__ import unicode_literals
 from frappe.model.document import Document
 
 class RoomType(Document):
-	pass
+  def before_save(self):
+    self.create_item_group()
+
+  def on_save(self):
+    pass
+
+  def create_item_group(self):
+  	if frappe.db.exists('Item Group', {'item_group_name': self.type}):
+       	# If the Room Type exists, update the item_group_name
+       	existing_item_group_name = frappe.get_doc('Item Group', {'item_group_name': self.type})
+       	existing_item_group_name.item_group_name = self.type
+       	existing_item_group_name.save()
+    else:
+       	# If the Room Type does not exist, create a new document and insert it
+       	item_group = frappe.new_doc('Item Group')
+       	item_group.item_group_name = self.type
+       	item_group.parent_item_group = "All Item Groups"
+       	item_group_name.insert()
+
+  
--- glistercp_hotel/glistercp_hotel/doctype/rooms/rooms.py
+++ glistercp_hotel/glistercp_hotel/doctype/rooms/rooms.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2020, Havenir and contributors
+# Copyright (c) 2024, Glistercp and Contributors
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
@@ -7,4 +7,25 @@ from __future__ import unicode_literals
 from frappe.model.document import Document
 
 class Rooms(Document):
-	pass
+  def before_save(self):
+    self.create_item()
+
+  def on_save(self):
+    pass
+
+  def create_item(self):
+  	if frappe.db.exists('Item', {'item_code': self.room_number}):
+       	# If the Room Type exists, update the item_group_name
+       	existing_item_name = frappe.get_doc('Item Group', {'item_group_name': self.room_number})
+       	existing_item_name.item_name = self.room_name
+       	existing_item_name.item_group = self.room_type
+       	existing_item_name.save()
+    else:
+       	# If the Room Type does not exist, create a new document and insert it
+       	item_name = frappe.new_doc('Item Group')
+       	item_name.item_code = self.room_number
+       	item_name.item_name = self.room_name
+       	item_name.item_group = self.room_type
+       	item_name.price = self.standard_rate
+       	item_name.is_stock_item = 0
+       	item_name.insert()
