ea35eeb1a75f7f37bea183fa226b69742aa16419
Adesina Akinyemi <59088328+adesinasam@users.noreply.github.com>
2024-03-15 21:28:24 +0100

    Update

--- glistercp_hotel/glistercp_hotel/doctype/hotel_check_in/hotel_check_in.js
+++ glistercp_hotel/glistercp_hotel/doctype/hotel_check_in/hotel_check_in.js
@@ -11,6 +11,14 @@ frappe.ui.form.on("Hotel Check In", {
         }
       }
     });
+    // setting query for customers to be visible in list
+    frm.set_query("customer", function(doc) {
+      return {
+        filters: {
+          customer_group: "All Customer Groups"
+        }
+      };
+    });
   },
   
   guest_id: function(frm){
@@ -27,11 +35,64 @@ frappe.ui.form.on("Hotel Check In", {
 
   validate: function(frm){
     for (var i in frm.doc.rooms){
-      if (frm.doc.rooms[i].male == 0 && frm.doc.rooms[i].female == 0){
+      if (frm.doc.rooms[i].male == 0 && frm.doc.rooms[i].female == 0 && frm.doc.rooms[i].children == 0){
         frappe.throw('Please Enter Guests Details for Room ' + frm.doc.rooms[i].room_no);
       }
     }
   },
+
+  to_date: function(frm) {
+    frm.call('calculate_stay_days').then(r => {
+      if (r.message) {
+        if (r.message > -1) {
+          var doc = frm.doc;
+          var days = 0;
+          if (r.message == 0) {
+            days = 1;
+          } else {
+            days = r.message;
+          }
+          // for (var i in doc.rooms) {
+          //   if (doc.rooms[i].room_no) {
+          //       doc.rooms[i].qty = days;
+          //       // doc.rooms[i].amount = days * doc.rooms[i].price;
+          //       frm.refresh_field("rooms");
+          //   }
+          // }
+          frm.doc.days = days;
+          frm.refresh_field('rooms');
+          frm.trigger("total_amount");
+        } else {
+          frm.doc.to_date = undefined;
+          frm.refresh_field("to_date");
+          frappe.msgprint("Expected Check out date cannot be before check in date.");
+        }
+      }
+    });
+  },
+
+  // to_date: function(frm) {
+  //     if (frm.doc.from_date && frm.doc.to_date) {
+  //         frm.call("calculate_stay_days", {
+  //             from_date: frm.doc.from_date,
+  //             to_date: frm.doc.to_date
+  //         }).then(r => {
+  //             if (r.message > -1) {
+  //                 var days = (r.message == 0) ? 1 : r.message;
+  //                 frm.doc.rooms.forEach(room => {
+  //                     if (room.room_no) {
+  //                         room.qty = days;
+  //                     }
+  //                 });
+  //                 frm.refresh_field("rooms");
+  //                 frm.trigger("total_amount");
+  //             } else {
+  //                 frm.set_value("to_date", undefined);
+  //                 frappe.msgprint("Expected Check out date cannot be before check in date.");
+  //             }
+  //         });
+  //     }
+  // },
 
   total_amount: function(frm){
     var temp_total_amount = 0;
@@ -67,13 +128,58 @@ frappe.ui.form.on('Hotel Check In Room', {
         frm.call('get_room_price',{room: row.room_no}).then( r => {
           row.price = r.message;
           frm.refresh_field('rooms')
+        });
+        frm.call('get_stay_days',{room: row.room_no}).then( r => {
+          row.qty = r.message;
+          frm.refresh_field('rooms')
         })
       }
     }
-    // frm.trigger('total_amount');
+    frm.trigger('total_amount');
   },
 
   rooms_remove: function(frm) {
-    // frm.trigger('total_amount');
+    frm.trigger('total_amount');
+  },
+
+
+  form_render: function(frm,cdt,cdn) {
+     let item = locals[cdt][cdn]; 
+     item.from_date = frm.doc.from_date;
+     item.to_date = frm.doc.to_date;
+     frm.refresh_field('rooms');
+  },
+
+  from_date: function(doc, cdt, cdn) {
+    var child = locals[cdt][cdn];
+    if (child.from_date < frappe.datetime.get_today()) {
+      child.from_date = "";
+      cur_frm.refresh_fields();
+      frappe.throw(__("You can not select past date"));
+    }
+  },
+  to_date: function(doc, cdt, cdn) {
+    var child = locals[cdt][cdn];
+    if (child.to_date < child.from_date) {
+      child.to_date = "";
+      cur_frm.refresh_fields();
+      frappe.throw(__("Invalid Check Out date"));
+    }
   }
+  // room_no: function(doc, cdt, cdn){
+  //   var child = locals[cdt][cdn];
+  //   frappe.call({
+  //     method: 'hotel_management.hotel_management.doctype.hotel_booking.hotel_booking.get_bookings',
+  //     args:{"start":child.from_date,
+  //     "end": child.to_date,
+  //     "room": child.room_no
+  //   },
+  //   callback: function(r){
+  //     /*child.from_date = "";
+  //     child.to_date = "";
+  //     child.room_no = "";*/
+  //     cur_frm.refresh_fields();
+  //   }
+  // });
+  // }
 })
--- glistercp_hotel/glistercp_hotel/doctype/hotel_check_in/hotel_check_in.py
+++ glistercp_hotel/glistercp_hotel/doctype/hotel_check_in/hotel_check_in.py
@@ -5,6 +5,7 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+from frappe.utils import add_days, date_diff, flt
 from frappe.core.doctype.sms_settings.sms_settings import send_sms
 
 
@@ -23,7 +24,21 @@ class HotelCheckIn(Document):
             room_doc = frappe.get_doc('Rooms', room.room_no)
             room_doc.db_set('check_in_id', self.name)
             room_doc.db_set('room_status', 'Checked In')
+        check_in_doc = frappe.get_doc('Hotel Check In', self.name)
+        all_checked_in = 1
         # send_payment_sms(self)
+
+        # Setting Check In doc to Complete
+        for room in check_in_doc.rooms:
+            if frappe.db.get_value('Rooms', room.room_no, 'room_status') == 'Available':
+                all_checked_in = 0
+
+        # Creating Additional Hotel Payment Vouchers
+        if self.amount_paid > 0 and self.customer == 'Hotel Walk In Customer':
+            self.create_payment_entry(self.amount_paid)
+        
+        # Creating Sales Invoice
+        self.create_sales_invoice(all_checked_in)
 
     def on_cancel(self):
         self.status = "Cancelled"
@@ -34,6 +49,80 @@ class HotelCheckIn(Document):
             room_doc.db_set('check_in_id', None)
             room_doc.db_set('room_status', 'Available')
 
+    def create_payment_entry(self, amount_paid, entry_type='Receive'):
+        payment_doc = frappe.new_doc('Hotel Payment Entry')
+        payment_doc.update({
+            'room': self.room,
+            'amount_paid': amount_paid,
+            'guest_id': self.guest_id,
+            'check_in_id': self.name,
+            'guest_name': self.guest_name,
+            'contact_no': self.contact_no
+            # 'contact_no': self.contact_no
+            # 'contact_no': self.contact_no
+            # 'contact_no': self.contact_no
+        })
+        payment_doc.save()
+        payment_doc.submit()
+
+    def create_sales_invoice(self, all_checked_in):
+        if self.customer == 'Hotel Walk In Customer':
+            self.create_invoice('Hotel Walk In Customer', self.name)
+
+        if all_checked_in == 1 or self.customer != 'Hotel Walk In Customer':
+            create_walk_in_invoice = any(item.is_pos == 1 for item in self.rooms)
+            if create_walk_in_invoice:
+                self.create_invoice('Hotel Walk In Customer', self.name)
+
+            check_out_list = frappe.get_list('Hotel Check In', filters={
+                'docstatus': 1,
+                'check_in_id': self.name,
+                'customer': ['not like', 'Hotel Walk In Customer']
+            }, order_by='name asc')
+            if all_checked_in == 1 and check_out_list:
+                self.create_invoice(check_out_list[0].customer, self.name)
+
+    def create_invoice(self, customer, check_in_id):
+        sales_invoice_doc = frappe.new_doc('Sales Invoice')
+        company = frappe.get_doc('Company', self.company)
+        sales_invoice_doc.update({
+            'customer': customer,
+            'check_in_id': check_in_id,
+            'guest_id': self.guest_id,
+            'check_in_date': frappe.get_value('Hotel Check In', check_in_id, 'check_in'),
+            'due_date': frappe.utils.data.today(),
+            'debit_to': company.default_receivable_account
+        })
+
+        for item in self.rooms:
+            item_doc = frappe.get_doc('Item', item.item)
+            default_income_account = company.default_income_account
+
+            if item.is_pos == 1:
+                default_income_account = item_doc.get_income_account()
+
+            sales_invoice_doc.append('rooms', {
+                'item_code': item_doc.item_code,
+                'item_name': item_doc.item_name,
+                'description': item_doc.description,
+                'qty': item.qty,
+                'uom': item_doc.stock_uom,
+                'price': item.rate,
+                'amount': item.amount,
+                'income_account': default_income_account
+            })
+
+        if self.discount:
+            sales_invoice_doc.discount_amount += self.discount
+
+        sales_invoice_doc.insert(ignore_permissions=True)
+        sales_invoice_doc.submit()
+
+    @frappe.whitelist()
+    def calculate_stay_days(self):
+        stay_days = date_diff(self.to_date, self.from_date)
+        return stay_days
+
     @frappe.whitelist()
     def get_room_price(self, room):
         room_price = frappe.get_value('Rooms', {
@@ -42,6 +131,7 @@ class HotelCheckIn(Document):
             'price'
         ])
         return room_price
+
 
 def send_payment_sms(self):
     sms_settings = frappe.get_doc('SMS Settings')
